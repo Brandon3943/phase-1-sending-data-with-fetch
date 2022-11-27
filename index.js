6929eb29b7c00f554cc93d1e35cf218058a02ce6
root <root@Ty-Laptop.localdomain>
2022-11-26 22:47:06 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Add your code here
+function submitData() {
+const formData = {
+    name: 'Steve',
+    email: 'steve@steve.com'
+  };
+  
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  const h1 = document.createElement('h1');
+  document.body.append(h1);
+  const h4 = document.createElement('h4');
+  document.body.append(h4);
+  
+function renderName(word) {
+  word.innerText = word;
+  h1.append(word);
+
+}
+
+function renderEmail(word) {
+  word.innerText = word;
+  h4.append(word);
+}
+
+  fetch("http://localhost:3000/users", configObj)
+    .then(responce => responce.json())
+    .then(data => {
+      renderName(data.name);
+      renderEmail(data.email);
+    })
+    .catch(error => {
+      alert('Error 304/404');
+      console.log(error);
+    })
+
+}
+    
+submitData();
